74872b2cf4d58db3aba0d496532c9be888191a79
Hudson678 <Hudsonmores31@gmail.com>
2024-10-01 13:12:54 -0300

    contagem regressiva

---
+++ Cregressiva.py
@@ -0,0 +1,16 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final.
+numero = 11
+while numero > 0:
+    numero = int(numero -1)
+    print (numero)
+if numero == 0:
+    print ("FIM!")
+
+
+
+
+
+
+
+
+    
